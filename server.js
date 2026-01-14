17472927b21d7aede4eaab8f55381caac02b6624
ChakriSriSaiPavan <145587697+ChakriSriSaiPavan@users.noreply.github.com>
2026-01-14 11:49:02 -0600

    Initial Project 5 test app

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Project 5 Test App – Elastic Beanstalk is running");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+const port = process.env.PORT || 80;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
